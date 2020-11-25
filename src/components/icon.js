2496b21b16d3bc5233bb6ba434356a95025ead46
KartikMahajan96 <66974780+Kartik-Mahajan@users.noreply.github.com>
2020-11-25 23:35:59 +0530

    Update icon.js

--- src/components/icon.js
+++ src/components/icon.js
@@ -1,26 +1,6 @@
 import React from "react";
 import {FaTimes, FaPen, FaRegCircle} from "react-icons/fa";
-// import {CgAdidas} from "react-icons/cg";
 
-// let Icon = ({name}) => {
-// 	return (
-// 		<div>
-// 			<p>
-// 				<FaTimes className="icon" />
-// 			</p>
-// 			<p>
-// 				{(name = "Pen")}
-// 				<FaPen className="icon" />
-// 			</p>
-// 			<p>
-// 				<FaRegCircle className="icon" />
-// 			</p>
-// 			<p>
-// 				<CgAdidas className="icon" />
-// 			</p>
-// 		</div>
-// 	);
-// };
 let Icon = ({name}) => {
 	switch (name) {
 		case "Circle":
